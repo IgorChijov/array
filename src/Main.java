f488e1a76db9c4e63a788ef656ad6b9cd3ec5c73
IgorChijov <igorchijof@gmail.com>
2024-05-12 23:15:59 +0300

    array-1-hw

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,67 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int [] numbers = new int[3];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        double [] secondNumbers = {1.57, 7.654, 9.986};
+        int [] temperature = {1, 3, 6, 5, 10};
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i]);
+            if (i < numbers.length - 1){
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        for (int i = 0; i < secondNumbers.length; i++) {
+            System.out.print(secondNumbers[i]);
+            if (i < secondNumbers.length - 1){
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        for (int i = 0; i < temperature.length; i++) {
+            System.out.print(temperature[i]);
+            if (i < temperature.length - 1){
+                System.out.print(", ");
+            }
+        }
+
+        System.out.println();
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            System.out.print(numbers[i]);
+            if (i > 0) {
+                System.out.print(", ");
+            }
+        }
+
+        System.out.println();
+        for (int i = secondNumbers.length - 1; i >= 0; i--) {
+            System.out.print(secondNumbers[i]);
+            if (i > 0) {
+                System.out.print(", ");
+            }
+        }
+
+        System.out.println();
+        for (int i = temperature.length - 1; i >= 0; i--) {
+            System.out.print(temperature[i]);
+            if (i > 0) {
+                System.out.print(", ");
+            }
+        }
+
+        System.out.println();
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 != 0) {
+                numbers[i] = numbers [i] + 1;
+            }
+        }
+        System.out.println(Arrays.toString(numbers));
     }
 }
