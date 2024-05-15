cbd781d25622f774ffe3e6b6b8c925d8518345ba
IgorChijov <igorchijof@gmail.com>
2024-05-15 12:19:23 +0300

    array-2-hw(корректировка)

--- src/Main.java
+++ src/Main.java
@@ -14,30 +14,29 @@ public class Main {
         int min = money[0];
         int max = money[0];
         for (int i = 0; i < money.length; i++) {
-            if (money[i] < min) {
-                min = money[i];
+            if (arr[i] < min) {
+                min = arr[i];
             }
-            if (money[i] > max) {
-                max = money[i];
+            if (arr[i] > max) {
+                max = arr[i];
             }
         }
         System.out.println("Минимальная сумма составила " + min + " , а максимальная " + max);
 
 
-        int[] arr1 = {123, 616, 5256, 9535, 10, 14};
+        int[] arr1 = {123, 616, 5256, 9535, 10};
         int sum1 = 0;
         for (int i = 0; i < arr1.length; i++) {
             sum1 += arr1[i];
         }
-        double average = sum1 / 4d;
+        double average = sum1 / (double) arr1.length;
         System.out.println("Средняя сумма затрат за месяц составила " + average);
+
 
         char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
         for (int i = reverseFullName.length - 1; i >= 0; i--) {
             System.out.print(reverseFullName[i]);
         }
         System.out.println();
-
-
     }
 }
